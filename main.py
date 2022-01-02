e62b4e55e8bf3a240a8e6f755970b11f8c22fbc1
Vlasta Rencova <rencovav@seznam.cz>
2022-01-02 13:08:00 +0100

    Added functionality (remove files) Created functions (DRY)

--- main.py
+++ main.py
@@ -1,66 +1,68 @@
 import argparse
-import sys
 import os.path
 from os import path
 from PIL import Image
 import shutil
 
+
+def create_sequence(sequence):
+    if sequence < 10:
+        return f"00{sequence}"
+    elif sequence < 100:
+        return f"0{sequence}"
+    else:
+        return f"{sequence}"
+
+
+def copy_file(input_path, output_path):
+    sequence = 1
+    filename = f"{output_path}{create_sequence(sequence)}.jpg"
+    while path.exists(filename):
+        sequence += 1
+        filename = f"{output_path}{create_sequence(sequence)}.jpg"
+    shutil.copyfile(input_path, filename)
+
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("input_dir")
 parser.add_argument("output_dir")
+parser.add_argument("-x", action="store_true")
 
 args = parser.parse_args()
 
 input_dir = args.input_dir
 output_dir = args.output_dir
 
-if input_dir is None:
-    print("Missing input_dir")
-    sys.exit()
-
-if output_dir is None:
-    print("Missing output_dir")
-    sys.exit()
 
 if not path.exists(input_dir):
     print("No input_dir")
 
-if not path.exists(output_dir):
-    os.makedirs(output_dir, exist_ok=False)
+remove_files = False
+if args.x:
+    if input("Do you want to remove the original files after copying?").lower() == "y":
+        remove_files = True
 
-for filename in os.listdir(input_dir):
-    absolute_path_input = os.path.join(input_dir, filename)
-    sequence_unknown = 1
-    new_filename_unknown = f"{sequence_unknown}.jpg"
+
+os.makedirs(output_dir, exist_ok=True)
+
+for file in os.listdir(input_dir):
+    absolute_path_input = os.path.join(input_dir, file)
+
     try:
         date_taken = Image.open(absolute_path_input)._getexif()[36867]
+        year_dir = date_taken[0:4]
+        month = date_taken[5:7]
+        day = date_taken[8:10]
+        new_filename = f"{year_dir}-{month}-{day}-"
+
     except KeyError:
-        os.makedirs(os.path.join(output_dir, "unknown"), exist_ok=True)
-        absolute_path_output = os.path.join(output_dir, "unknown", new_filename_unknown)
+        year_dir = "unknown"
+        new_filename = ""
 
-        while path.exists(absolute_path_output):
-            sequence_unknown += 1
-            new_filename_unknown = f"{sequence_unknown}.jpg"
-            absolute_path_output = os.path.join(output_dir, "unknown", new_filename_unknown)
+    os.makedirs(os.path.join(output_dir, year_dir), exist_ok=True)
+    absolute_path_output = os.path.join(output_dir, year_dir, new_filename)
 
-        shutil.copyfile(absolute_path_input, os.path.join(output_dir, "unknown", new_filename_unknown))
-        continue
-
-    year = date_taken[0:4]
-
-    month = date_taken[5:7]
-    day = date_taken[8:10]
-    sequence = 1
-
-    new_filename = f"{year}-{month}-{day}-{sequence}.jpg"
-
-    absolute_path_output = os.path.join(output_dir, year, new_filename)
-
-    while path.exists(absolute_path_output):
-        sequence += 1
-        new_filename = f"{year}-{month}-{day}-{sequence}.jpg"
-        absolute_path_output = os.path.join(output_dir, year, new_filename)
-
-    os.makedirs(os.path.join(output_dir, year), exist_ok=True)
-    shutil.copyfile(absolute_path_input, absolute_path_output)
+    copy_file(absolute_path_input, absolute_path_output)
+    if remove_files:
+        os.remove(absolute_path_input)
